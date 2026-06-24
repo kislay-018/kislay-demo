d9d046d57a91f07b5ea84ebb53db8311f6e21b04
kislay <kislaykumar@kislays-MacBook-Air.local>
2026-06-24 20:33:58 +0530

    add new files

---
+++ todolist.js
@@ -0,0 +1,37 @@
+let todo=[];
+
+let req=prompt("enter a request");
+
+while(true){
+    if(req== "quit"){
+        console.log("quitting app");
+        break;
+    }
+    if(req=="list"){
+        console.log("---------");
+        for(let i=0;i<todo.length;i++) {
+            console.log(i,todo[i]);
+
+        }
+        console.log("--------");
+
+        
+    } else if(req=="add"){
+         let task=prompt("please enter the task  that you want to add ");
+         todo.push(task);
+         console.log("task added");
+
+    } else if(req=="delete"){
+        let idx = prompt("please enter the task that y0u want to add");
+        todo.splice(idx,1);
+        console.log("task deleted");
+    } else{
+        console.log("wrong request"); 
+    }
+
+    req=prompt("enter a request");
+
+}
+
+ 
+
